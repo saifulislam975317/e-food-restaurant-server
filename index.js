31ec3f54a349bc53cd2784a63507e7e554154b20
saiful islam <saifulislam975317@gmail.com>
2023-08-24 02:22:42 +0600

    server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-var jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -36,18 +36,18 @@ async function run() {
     // Middleware verify jwt token
 
     const verifyJwt = (req, res, next) => {
-      const authorization = req.headers.authorization;
-      if (!authorization) {
+      const authToken = req.headers.authorization;
+
+      if (!authToken) {
         return res
           .status(401)
           .send({ error: true, message: "unauthorized access" });
       }
-      const token = authorization.split(" ")[1];
+      const token = authToken.split(" ")[1];
+
       jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
         if (err) {
-          return res
-            .status(401)
-            .send({ error: true, message: "unauthorized access" });
+          return res.status(403).send({ message: "forbidden access" });
         }
         req.decoded = decoded;
         next();
@@ -58,7 +58,7 @@ async function run() {
 
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign({ user }, process.env.ACCESS_TOKEN, {
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
         expiresIn: "1d",
       });
       res.send({ token });
@@ -84,8 +84,8 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/cart", verifyJwt, async (req, res) => {
-      const email = req.query.email;
+    app.get("/cart/:email", verifyJwt, async (req, res) => {
+      const email = req.params.email;
       if (!email) {
         return [];
       }
@@ -145,14 +145,16 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users/admin/:email", async (req, res) => {
+    app.get("/users/admin/:email", verifyJwt, async (req, res) => {
       const email = req.params.email;
-
-      // if (email !== req.decode.email) {
-      //   return res
-      //     .status(403)
-      //     .send({ error: true, message: "forbidden access" });
-      // }
+      console.log("normal email", email);
+      const decodedEmail = req.decoded.email;
+      console.log("decoded email", decodedEmail);
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
+      }
       const query = { email: email };
       const user = await userCollection.findOne(query);
       const result = { admin: user?.role === "admin" };
